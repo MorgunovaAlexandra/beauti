d312f3e070b370e2d20ea4a44795cd38fa64e07b
мордвинова александра <90259849+MordvinovaAlexandra@users.noreply.github.com>
2022-08-25 12:30:23 +0300

    first

---
+++ .history/slider_20220825111426.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+	
+	
+	$('.slider').slick({
+		arrows:true,
+		adaptiveHeight:true,
+		slidesToShow:3,
+		touchMove:true,
+		responsive:[
+			{
+				breakpoint:768,
+				settings:{slidesToShow:3}
+			},
+			{
+				breakpoint:500,
+				settings:{slidesToShow:1}
+			},
+			{
+				breakpoint:414,
+				settings:{slidesToShow:1}
+			},
+			{
+				breakpoint:320,
+				settings:{slidesToShow:1}
+			}
+
+		]
+	})
+});
---
+++ .history/slider_20220825111834.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+	$('.slider').slick({
+		arrows:true,
+		adaptiveHeight:true,
+		slidesToShow:2,
+		touchMove:true,
+		responsive:[
+			{
+				breakpoint:768,
+				settings:{slidesToShow:1}
+			},
+			{
+				breakpoint:500,
+				settings:{slidesToShow:1}
+			},
+			{
+				breakpoint:414,
+				settings:{slidesToShow:1}
+			},
+			{
+				breakpoint:320,
+				settings:{slidesToShow:1}
+			}
+
+		]
+	})
+});
